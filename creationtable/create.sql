40c7919b7f6e37b6becfcd98e74baf58653d08aa
Julien Dauguet <Brooked@202.143.etud.wifi.u-picardie.fr>
2017-03-28 12:03:28 +0200

    Premiere version du create

---
+++ creationtable/create.sql
@@ -0,0 +1,29 @@
+use Championnat;
+
+CREATE TABLE entraineur ( 
+    id_entraineur integer NOT NULL, 
+    id_equipe char(8) NOT NULL 
+); 
+CREATE TABLE equipe ( 
+    id_equipe char(3) NOT NULL, 
+    nom_equipe varchar(50), 
+    id_pays integer 
+); 
+CREATE TABLE joueur ( 
+    id_joueur integer NOT NULL, 
+    id_equipe char(3) 
+); 
+CREATE TABLE personne ( 
+    id_personne integer NOT NULL, 
+    prenom_personne varchar(50), 
+    nom_personne varchar(50) 
+); 
+CREATE TABLE officiel ( 
+    id_officiel integer NOT NULL, 
+    id_pays integer 
+); 
+CREATE TABLE pays ( 
+    id_pays integer NOT NULL, 
+    nom_pays varchar(50), 
+    iso char(3) 
+); 
